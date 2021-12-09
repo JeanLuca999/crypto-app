834ac4f958aaaab63ed825a516a77bc2229f999f
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-12-09 10:26:23 -0300

    Cria estrutura inicial para chamada na api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
 import './style.css'
 
 export default function Home() {
+  const [coinsList, setCoinsList] = useState([])
+
+  async function getCoinsList() {
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    const response = await fetch(url)
+    const responseData = await response.json()
+    setCoinsList(responseData)
+  }
+
+  useEffect(() => {
+    getCoinsList()
+  }, [])
+
   return (
     <main className="home">
-      <h1 className="home__title">Coin Checker</h1>
+      <h1 className="home__title">Crypto App</h1>
+      <div className="home__search-container">
+        <input type="text" placeholder="Bitcoin" className="home__input"></input>
+      </div>
     </main>
   )
 }
