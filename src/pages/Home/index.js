dacff9a53fae2c08251ba9fe7098d25be382f124
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-12-09 12:07:38 -0300

    Implementa função de filtro na lista de coins

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,23 @@ import './style.css'
 
 export default function Home() {
   const [coinsList, setCoinsList] = useState([])
+  const [allCoins, setAllCoins] = useState([])
+  const [inputText, setInputText] = useState('')
+
+  console.log(allCoins)
 
   async function getCoinsList() {
-    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=10&page=1&sparkline=false'
     const response = await fetch(url)
     const responseData = await response.json()
+
     setCoinsList(responseData)
+    setAllCoins(responseData)
+  }
+
+  function filterCoinsList(text) {
+    const filteredArray = allCoins.filter(e => e.name.toLowerCase().includes(text.toLowerCase()))
+    setCoinsList(filteredArray)
   }
 
   useEffect(() => {
@@ -20,7 +31,13 @@ export default function Home() {
     <main className="home">
       <h1 className="home__title">Crypto App</h1>
       <div className="home__search-container">
-        <input type="text" placeholder="Bitcoin" className="home__input"></input>
+        <input
+          value={inputText}
+          type="text"
+          placeholder="Bitcoin"
+          className="home__input" 
+          onChange={e => {setInputText(e.target.value); filterCoinsList(e.target.value)}}
+        ></input>
       </div>
       <section>
         <ul className="home__list">
