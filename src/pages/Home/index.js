ea2b0678b41d707be72bbe901acfdaa5cc4c91fb
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-12-09 13:25:25 -0300

    Aumenta o valor de itens por página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
 
   async function getCoinsList() {
-    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=15&page=${page || 1}&sparkline=false`
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=25&page=${page || 1}&sparkline=false`
     const response = await fetch(url)
     const responseData = await response.json()
 
