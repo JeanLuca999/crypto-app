03de958ea56882190d77dbd9425484a3bac1d3a6
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-12-10 14:18:10 -0300

    Criação dos arquivos  inicial

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <>
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Home/>}  />
+        <Route path="/" element={<Home/>}  />
         <Route path="/:page" element={<Home/>} />
       </Routes>
     </BrowserRouter>
